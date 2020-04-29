d435eb90b06f57082708f386a354d7b588f38fd0
zuqq <noahheld17@gmail.com>
2020-04-29 11:41:11 +0200

    Variable and type variable indices get newtype wrappers

--- src/Lambda/Inference.hs
+++ src/Lambda/Inference.hs
@@ -1,59 +1,51 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Lambda.Inference where
 
 import Control.Monad.Trans.State.Strict (gets, modify, runState, State)
-import qualified Data.IntMap.Strict     as IM
-import qualified Data.IntSet            as IS
+import qualified Data.Map.Strict        as M
+import qualified Data.Set               as S
 
 
 -- Terms
 
+newtype VIx = VIx Int
+    deriving (Eq, Num, Ord, Read, Show)
+
 -- | Terms are those of the untyped lambda calculus.
-data Term = Var Int | Abs Term | App Term Term
+data Term = Var VIx | Abs Term | App Term Term
     deriving (Eq, Read, Show)
 
 -- Types
 
+newtype TVIx = TVIx Int
+    deriving (Eq, Num, Ord, Read, Show)
+
 -- | A type is either a type variable or a function type. There is an infinite
 -- number of type variables, indexed by the natural numbers.
-data Type = TVar Int | TArr Type Type
+data Type = TVar TVIx | TArr Type Type
     deriving (Eq, Read, Show)
 
 -- | Map a type to the set of its type variables.
-tVars :: Type -> IS.IntSet
-tVars (TVar n)   = IS.singleton n
-tVars (TArr a b) = IS.union (tVars a) (tVars b)
+tVars :: Type -> S.Set TVIx
+tVars (TVar n)   = S.singleton n
+tVars (TArr a b) = S.union (tVars a) (tVars b)
 
 -- Typing context
 
 -- | A typing context is a finite mapping from free variables to types.
---
--- Because we are using de Bruijn indices, the typing context for the body of an
--- abstraction derives from a shift of the typing context for the abstraction.
--- Instead of creating a new mapping, we record the distance from the outermost
--- scope in the field 'depth' and only shift during lookups.
-data Context = Context
-    { depth :: Int             -- ^ Distance from the outermost scope.
-    , table :: IM.IntMap Type  -- ^ Map from type variable indices to types.
-    }
-    deriving (Eq, Read, Show)
-
--- | Look up the type of a free variable.
-find :: Int -> Context -> Maybe Type
-find n c = IM.lookup (n - depth c) (table c)
+type Context = M.Map VIx Type
 
 -- | Find the largest type variable index that is mentioned in a context.
-maxTVar :: Context -> Int
-maxTVar c = foldr (max . IS.findMax . tVars) 0 (table c)
+maxTVar :: Context -> TVIx
+maxTVar = foldr (max . S.findMax . tVars) 0
 
 -- | Adjust the context when going under an abstraction.
 bind
     :: Type     -- ^ Type for the bound variable of the abstraction
     -> Context  -- ^ Context for the abstraction.
     -> Context  -- ^ Context for the body of the abstraction.
-bind a c = Context
-    { depth = depth c + 1
-    , table = IM.insert (-(depth c + 1)) a (table c)
-    }
+bind a = M.insert 0 a . M.mapKeys (+ 1)
 
 -- Gathering constraints
 
@@ -62,7 +54,7 @@ data Constr = CEq Type Type
     deriving (Eq, Read, Show)
 
 data GatherState = GatherState
-    { index :: Int       -- ^ Fresh type variable index.
+    { index :: TVIx      -- ^ Fresh type variable index.
     , acc   :: [Constr]  -- ^ List of collected constraints.
     }
     deriving (Eq, Read, Show)
@@ -84,7 +76,7 @@ record constr = modify $ \s -> s { acc = constr : acc s }
 -- typed by the context get fresh type variables, keeping their types as
 -- general as possible.
 gather :: Context -> Term -> Gather Type
-gather c (Var n) = maybe freshTVar return (find n c)
+gather c (Var n) = maybe freshTVar return (M.lookup n c)
 gather c (Abs t) = do
     x <- freshTVar
     b <- gather (bind x c) t
@@ -103,11 +95,11 @@ runGather c t = (a, acc s')
 
 -- Substitutions
 
-type Sub = IM.IntMap Type
+type Sub = M.Map TVIx Type
 
 -- | Apply a substitution to a type.
 apply :: Sub -> Type -> Type
-apply s (TVar n)   = IM.findWithDefault (TVar n) n s
+apply s (TVar n)   = M.findWithDefault (TVar n) n s
 apply s (TArr a b) = TArr (apply s a) (apply s b)
 
 -- | Apply a substitution to both sides of a constraint.
@@ -120,7 +112,7 @@ applyBoth s (CEq a b) = CEq (apply s a) (apply s b)
 -- @
 -- for every @a@.
 after :: Sub -> Sub -> Sub
-after s' s = IM.union (IM.map (apply s') s) s'
+after s' s = M.union (M.map (apply s') s) s'
 
 -- Solving constraints
 
@@ -129,14 +121,14 @@ data RecException = RecException Type Type
 -- | Try to find a substitution that solves the given constraints. The only way
 -- for this to fail is if it encounters a recursive constraint.
 unify :: [Constr] -> Either RecException Sub
-unify []        = Right IM.empty
+unify []        = Right M.empty
 unify ((CEq a b) : rest)
     | a == b    = unify rest
     | otherwise = case (a, b) of
         (TVar m, b)
-            | m `IS.member` tVars b -> Left $ RecException a b
+            | m `S.member` tVars b -> Left $ RecException a b
             | otherwise             -> do
-                let s = IM.singleton m b
+                let s = M.singleton m b
                 s' <- unify (map (applyBoth s) rest)
                 return $ s' `after` s
         (a, TVar n)                 -> unify $ CEq b a : rest
